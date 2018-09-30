a30f9cb3f02dffd01e9b9daf8c1b92a4599e7f2a
Rafael Gumieri <rgumieri@gmail.com>
2018-09-29 22:16:40 -0300

    Add print and error methods

--- typist.go
+++ typist.go
@@ -14,10 +14,11 @@ type Typist struct {
 	Quiet bool
 	Out   io.Writer
 	In    io.Reader
+	Err   io.Writer
 }
 
-// Type printing information according Typist configurations
-func (t *Typist) Type(format string, a ...interface{}) (n int, err error) {
+// Printf printing information according Typist configurations
+func (t *Typist) Printf(format string, a ...interface{}) (n int, err error) {
 	if t.Quiet {
 		return
 	}
@@ -30,8 +31,22 @@ func (t *Typist) Type(format string, a ...interface{}) (n int, err error) {
 	return fmt.Fprintf(output, format, a...)
 }
 
-// ReadLine wait from break-line input and return as string
-func (t *Typist) ReadLine() (string, error) {
+// Println printing information according Typist configurations
+func (t *Typist) Println(a ...interface{}) (n int, err error) {
+	if t.Quiet {
+		return
+	}
+
+	output := t.Out
+	if output == nil {
+		output = os.Stdout
+	}
+
+	return fmt.Fprintln(output, a...)
+}
+
+// Readln wait from break-line input and return as string
+func (t *Typist) Readln() (string, error) {
 	input := t.In
 	if input == nil {
 		input = os.Stdin
@@ -47,13 +62,54 @@ func (t *Typist) Confirm(message string) bool {
 		return false
 	}
 
-	t.Type("%s [y/N] ", message)
+	t.Printf("%s [y/N] ", message)
 
-	response, err := t.ReadLine()
+	response, err := t.Readln()
 	if err != nil {
 		return false
 	}
 
 	response = strings.ToLower(strings.TrimSpace(response))
 	return response == "y" || response == "yes"
+}
+
+// Errorf print strings to defined Err output
+func (t *Typist) Errorf(format string, a ...interface{}) (n int, err error) {
+	output := t.Err
+	if output == nil {
+		output = os.Stderr
+	}
+
+	return fmt.Fprintf(output, format, a...)
+}
+
+// Errorln print strings to defined Err output
+func (t *Typist) Errorln(a ...interface{}) (n int, err error) {
+	output := t.Err
+	if output == nil {
+		output = os.Stderr
+	}
+
+	return fmt.Fprintln(output, a...)
+}
+
+// Must checks for error.
+// if not nil it exit the process according the configurations
+func (t *Typist) Must(err error) {
+	if err == nil {
+		return
+	}
+
+	t.Errorln(err.Error())
+	t.Finish(err)
+}
+
+// Finish exit the process with (TODO) the right ERRNO code
+func (t *Typist) Finish(err error) {
+	switch err {
+	case nil:
+		os.Exit(0)
+	default:
+		os.Exit(1)
+	}
 }
